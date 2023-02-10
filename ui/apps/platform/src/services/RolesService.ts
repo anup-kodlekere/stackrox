aa1ae251ecdbce3845209861678b6a977f23dd84
Yann Brillouet <ybrillou@redhat.com>
2023-02-10 18:49:44 +0100

    Style fixes

--- ui/apps/platform/src/services/RolesService.ts
+++ ui/apps/platform/src/services/RolesService.ts
@@ -152,11 +152,11 @@ type NamespacesForClusterAndPermissionsRequest = {
 export type NamespaceForClusterAndPermissions = {
     id: string;
     name: string;
-}
+};
 
 export type NamespacesForClusterAndPermissionsResponse = {
     namespaces: NamespaceForClusterAndPermissions[];
-}
+};
 
 export function getNamespacesForClusterAndPermissions(
     clusterID: string,
@@ -169,4 +169,3 @@ export function getNamespacesForClusterAndPermissions(
         .get<NamespacesForClusterAndPermissionsResponse>(targetUrl)
         .then((response) => response.data);
 }
-
